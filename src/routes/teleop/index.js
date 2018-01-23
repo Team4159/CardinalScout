5223755ba9b70867cf66f4b7697a38a65fa213bf
chasenheimer <chasenheimer@gmail.com>
2018-01-22 17:57:34 -0800

    Style Teleop

--- src/routes/teleop/index.js
+++ src/routes/teleop/index.js
@@ -2,27 +2,37 @@ import { h } from "preact";
 import Button from "preact-material-components/Button";
 import "preact-material-components/Button/style.css";
 import "preact-material-components/Theme/style.css";
+import style from "./style.css";
+
 const Teleop = ({}) => (
-  <div>
+  <div className={style.Teleop}>
     <h1> teleop </h1>
     <h2> Pickup </h2>
-    <Button raised>field</Button>
-    <Button raised>pyramid</Button>
-    <Button raised>portal</Button>
+    <div className={style.wrapper}>
+      <Button raised>field</Button>
+      <Button raised>pyramid</Button>
+      <Button raised>portal</Button>
+    </div>
     <h3>Dropoff</h3>
-    <Button raised>scale</Button>
-    <Button raised>switch</Button>
-    <Button raised>vault</Button>
-    <Button raised>fail</Button>
-    <Button raised>fail</Button>
-    <Button raised>fail</Button>
+    <div className={style.wrapper}>
+      <Button raised>scale</Button>
+      <Button raised>switch</Button>
+      <Button raised>vault</Button>
+    </div>
+    <div className={style.wrapper}>
+      <Button raised>fail</Button>
+      <Button raised>fail</Button>
+      <Button raised>fail</Button>
+    </div>
     <Button raised>epic fail</Button>
     <h4>How fast was their pickup?(1 = instant, 5 = 5 or more seconds)</h4>
-    <Button raised>1</Button>
-    <Button raised>2</Button>
-    <Button raised>3</Button>
-    <Button raised>4</Button>
-    <Button raised>5</Button>
+    <div className={style.wrapper}>
+      <Button raised>1</Button>
+      <Button raised>2</Button>
+      <Button raised>3</Button>
+      <Button raised>4</Button>
+      <Button raised>5</Button>
+    </div>
   </div>
 );
 
